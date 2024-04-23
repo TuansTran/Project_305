2086f62e41e90d298c680773b63164623bfbe046
TuansTran <tuan.trananh.cit21@eiu.edu.vn>
2024-04-23 16:06:20 +0700

    4.0

--- User.java
+++ User.java
@@ -1,57 +1,60 @@
 public class User {
-    private String UserID;
+    private String userID;
     private String password;
     private String email;
-    private String userType;
+    private UserType userType;
 
-    public User (String id, String pass, String mail, String type){
-        UserID = id;
-        password = pass;
-        email= mail;
-        userType = type;
+    public static enum UserType {
+        TENANT,
+        MANAGER,
+        OWNER
+    }
+
+    public User(String userID, String password, String email, UserType userType) {
+        this.userID = userID;
+        this.password = password;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public User createUser(String userID, String password, String email, UserType userType) {
+        return new User(userID, password, email, userType);
+    }
+
+    public void resetPassword(String newPassword) {
+        password = newPassword;
     }
 
     public String getUserID() {
-        return UserID;
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserID(String userID) {
-        UserID = userID;
-    }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setUserType(String userType) {
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
         this.userType = userType;
-    }
-
-    public void createUser(String id, String pass, String mail, String type){
-        UserID=id;
-        password=pass;
-        email=mail;
-        userType=type;
-    }
-
-    public void changePassword(String newPass){
-        password=newPass;
     }
 
 }
